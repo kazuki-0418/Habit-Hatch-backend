b8f526f903207a24d440599b0759f591ee812949
kazuki-0418 <kazuki.castle0418@gmail.com>
2023-09-07 12:57:06 +0900

    eslint&prettier設定

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,9 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['import-access', 'prettier', '@typescript-eslint/eslint-plugin'],
   extends: [
+    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
@@ -21,5 +22,49 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/consistent-type-imports': ['warn', { prefer: 'type-imports' }],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { varsIgnorePattern: '^_', argsIgnorePattern: '^_' },
+    ],
+    '@typescript-eslint/naming-convention': [
+      'error',
+      { selector: ['typeAlias', 'typeParameter'], format: ['PascalCase'] },
+      {
+        // 変数
+        selector: ['variable'],
+        format: ['strictCamelCase'],
+      },
+      {
+        // 定数
+        selector: ['variable'],
+        modifiers: ['const'],
+        format: ['strictCamelCase', 'UPPER_CASE'],
+      },
+      {
+        // 関数、コンポーネント
+        selector: ['variable'],
+        types: ['function'],
+        modifiers: ['const'],
+        format: ['strictCamelCase', 'PascalCase'],
+      },
+      {
+        selector: 'parameter',
+        format: ['strictCamelCase'],
+        leadingUnderscore: 'allow',
+      },
+      {
+        selector: ['typeAlias', 'interface'],
+        format: ['StrictPascalCase'],
+      },
+      {
+        selector: 'variable',
+        types: ['boolean'],
+        format: ['PascalCase'],
+        prefix: ['no', 'is', 'has', 'should'],
+        filter: { regex: '^_', match: false },
+      },
+    ],
   },
-};
+}
